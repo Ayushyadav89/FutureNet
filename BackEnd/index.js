22369037b0b970c03b6216eabcae11db0dcb9671
Ayushyadav89 <2k22.cse.2213307@gmail.com>
2025-04-02 21:09:55 +0530

    connect with backend

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,16 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express()
 
 dotenv.config();
 
 const port = process.env.PORT || 4000;
+const URI = process.env.mongoDBURI;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// connect to mongoDB
+try {
+  mongoose.connect(URI);
+  console.log("Connected To MongoDB");
+}
+catch (error) {
+  console.log("Error:", error);
+  process.exit(1);
+}
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`server listning on https://localhost:${port}`)
 })
